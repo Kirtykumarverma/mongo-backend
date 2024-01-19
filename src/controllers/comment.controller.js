11c64ab68965a0cca0b31b05f15e24783732e0ae
Kirty Kumar Verma <32432876+Kirtykumarverma@users.noreply.github.com>
2024-01-19 21:18:45 +0530

    Add Types import in comment.controller.js and add deleteFromCloudinary function in Cloudinary.js

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import mongoose, { Types, isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -8,6 +8,30 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  console.log("GET ALL COMMENTS : " + videoId);
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video");
+  }
+
+  const aggregate = Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+  ]);
+
+  Comment.aggregatePaginate(aggregate, { page, limit })
+    .then((result) => {
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(200, result, "All comments fetched successfully")
+        );
+    })
+    .catch((err) => err?.message || "ERROR");
 });
 
 const addComment = asyncHandler(async (req, res) => {
@@ -70,6 +94,15 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+  const { commentId } = req.params;
+  if (!isValidObjectId(commentId))
+    throw new ApiError(400, "Please enter a valid comment Id");
+
+  const commentDelete = await Comment.findByIdAndDelete(commentId);
+  if (!commentDelete)
+    throw new ApiError(400, "Something went wrong while deleting the comment");
+
+  return res.status(200).json(new ApiResponse(200, {}, "Deleted Successfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
